bdc213b76eee81a4d5d8e3df5041bf33380cf66f
Sveta Golovko <dreamsaboutlana@gmail.com>
2017-10-24 23:21:28 +0300

    Format file

--- nodeJS/hw2/main.js
+++ nodeJS/hw2/main.js
@@ -1,8 +1,8 @@
 'use strict';
 const fs = require('file-system');
 
-fs.writeFile("test_folder/test.txt", new Date().toLocaleDateString(), function(e) {
-  if(e) {
+fs.writeFile("test_folder/test.txt", new Date().toLocaleDateString(), function (e) {
+  if (e) {
     return console.log(e);
   }
   console.log("The file was saved in new folder!");
